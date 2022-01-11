d9b7ae3d265e9eabafa16d491fe701faad631bbb
alinblendea <alinblendea1708@gmail.com>
2022-01-12 00:01:43 +0200

    First half of the project
    Added the first 3 pages of the project, as well as the infrastructure of the other 3 and the night mode functionality.

---
+++ scripts.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+
+    darkmode = document.getElementById("check1");
+    hidden = document.getElementById("check2");
+    page = document.getElementsByClassName("pagina")[0];
+
+    function colors(color, colorPage) {
+        document.body.style.background = color;
+        page.style.background = colorPage;
+    }
+    darkmode.addEventListener("click", e => {
+        if (darkmode.checked) {
+            colors("#0c0d0e", "#3d3d3d")
+            page.style.color = "white";
+        } else {
+            colors("aliceblue", "#dddee7")
+            page.style.color = "black";
+        }
+    })
+}
