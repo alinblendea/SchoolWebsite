f072dae8dff7296a69b422f43d942d7a6cd75876
alinblendea <alinblendea1708@gmail.com>
2022-01-12 01:11:11 +0200

    Fixed night mode bug
    Fixed a night mode bug

--- scripts.js
+++ scripts.js
@@ -2,18 +2,31 @@ window.onload = function() {
 
     darkmode = document.getElementById("check1");
     page = document.getElementsByClassName("pagina")[0];
+    nightText = document.getElementById("night");
+    nightTitle = document.getElementById("nightTitle");
 
     function colors(color, colorPage) {
         document.body.style.background = color;
-        page.style.background = colorPage;
+        if(page != null)
+            page.style.background = colorPage;
     }
     darkmode.addEventListener("click", e => {
         if (darkmode.checked) {
             colors("#0c0d0e", "#3d3d3d")
-            page.style.color = "white";
+            if(page != null)
+                page.style.color = "white";
+            if(nightText != null)
+                nightText.style.color = "white";
+            if(nightTitle != null)
+                nightTitle.style.color = "white";
         } else {
             colors("aliceblue", "#dddee7")
-            page.style.color = "black";
+            if(page != null)
+                page.style.color = "black";
+            if(nightText != null)
+                nightText.style.color = "#041664";
+            if(nightTitle != null)
+                nightTitle.style.color = "#041664";
         }
     })
 }
